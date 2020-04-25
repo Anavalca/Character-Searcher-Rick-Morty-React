c046d75bb00eb2c466110029ff6db306daccd6a6
AnaValca <avc92j@gmail.com>
2020-04-25 18:33:35 +0200

    docs

---
+++ docs/precache-manifest.fea726dfb5b52af05fc40e3c8688ca89.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90081f4e5f98c2a1d781bb140cb6b525",
+    "url": "./index.html"
+  },
+  {
+    "revision": "450a41e4b1baffdb48ed",
+    "url": "./static/css/main.82906885.chunk.css"
+  },
+  {
+    "revision": "3995bd3bf8ee77ce4074",
+    "url": "./static/js/2.7970e2ed.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.7970e2ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "450a41e4b1baffdb48ed",
+    "url": "./static/js/main.964afe1d.chunk.js"
+  },
+  {
+    "revision": "c70c400507ac0d03dba1",
+    "url": "./static/js/runtime-main.793ca9c6.js"
+  },
+  {
+    "revision": "8aca237cca4835644ca8c99f7205577a",
+    "url": "./static/media/2.8aca237c.jpg"
+  },
+  {
+    "revision": "c14a633a0f4a7ad7c6e43a3f05cc0e9e",
+    "url": "./static/media/3.c14a633a.jpg"
+  },
+  {
+    "revision": "5a4c440ec8631d2fc16a3936d451bec9",
+    "url": "./static/media/4.5a4c440e.jpg"
+  },
+  {
+    "revision": "52a1f1514c395e3b34e36b1d415abc5e",
+    "url": "./static/media/5.52a1f151.jpg"
+  },
+  {
+    "revision": "731f142cec859be1a9724c981af20f16",
+    "url": "./static/media/MortySad.731f142c.gif"
+  },
+  {
+    "revision": "5f389cd248d17b6cb04d217e93376f73",
+    "url": "./static/media/ThemeSong.5f389cd2.mp3"
+  },
+  {
+    "revision": "7c809d71fee99a69069a1cda1d817be4",
+    "url": "./static/media/alien.7c809d71.png"
+  },
+  {
+    "revision": "5f6a585afa2f7b6d753f3861ef87570c",
+    "url": "./static/media/chat.5f6a585a.png"
+  },
+  {
+    "revision": "7956fff12536275144cb8a88cc882066",
+    "url": "./static/media/death.7956fff1.png"
+  },
+  {
+    "revision": "26e09ce454dd54e0e6b341cc5a8bd871",
+    "url": "./static/media/fondoStart.26e09ce4.webp"
+  },
+  {
+    "revision": "375f577dbe1b7c767326f566e547a663",
+    "url": "./static/media/heart.375f577d.png"
+  },
+  {
+    "revision": "1df0613ee81f2168baab418cca5ebc12",
+    "url": "./static/media/human.1df0613e.png"
+  },
+  {
+    "revision": "562a569c9d3dd178ec49817776ee062e",
+    "url": "./static/media/otherUniverse.562a569c.png"
+  },
+  {
+    "revision": "cac01c7b8559eeb65a041e27494acbb5",
+    "url": "./static/media/pickle.cac01c7b.gif"
+  },
+  {
+    "revision": "16ef7f7766bf81b1260710869361f56c",
+    "url": "./static/media/portal.16ef7f77.gif"
+  }
+]);
